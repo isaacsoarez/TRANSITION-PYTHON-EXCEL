e64c6b6b4034d91eb0e91543a5f53ca1e382dd33
Isaac Soares <132666435+isaacsoarez@users.noreply.github.com>
2023-07-28 15:50:28 -0300

    Add files via upload

--- app2.py
+++ app2.py
@@ -15,6 +15,16 @@ for rows in frutas_page.iter_rows(min_row=2, max_row=4):
 
 for rows in frutas_page.iter_rows(min_row=2, max_row=4):
     print(rows[0].value, rows[1].value, rows[2].value)
+
+for rows in frutas_page.iter_rows(min_row=2, max_row=4):
+    for cell in rows:
+        if cell.value == "Banana":
+            cell.value = "Frutas 01"
+
+book.save("planilha de Compras.xlsx")
+
+book.save("planilha de Compras v2.xlsx")
+
 #----------------------------------------------------------
 book["bebidas"]
 
@@ -27,3 +37,13 @@ for rows in bebidas_page.iter_rows(min_row=2, max_row=4):
 
 for rows in bebidas_page.iter_rows(min_row=2, max_row=4):
     print(rows[0].value, rows[1].value, rows[2].value)
+
+    for rows in bebidas_page.iter_rows(min_row=2, max_row=4):
+        for cell in rows:
+            if cell.value == "Coca-Cola":
+                cell.value = "bebidas 01"
+
+    book.save("planilha de Compras.xlsx")
+
+    book.save("planilha de Compras v2.xlsx")
+
