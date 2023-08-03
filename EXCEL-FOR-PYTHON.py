4469b7f5f0bd3ec05d8c086301546c5f2215bdd9
Isaac Soares <132666435+isaacsoarez@users.noreply.github.com>
2023-08-03 15:21:37 -0300

    Update EXCEL-FOR-PYTHON.py

--- EXCEL-FOR-PYTHON.py
+++ EXCEL-FOR-PYTHON.py
@@ -1,26 +1,48 @@
-import openpyxl
+import openpyxl, PatternFill
 
-# 1° PASSO (CARREGAR ARQUIVO)
-book = openpyxl.load_workbook('DADOS.xlsx')
+openpyxl.Workbook()
 
-# 2° PASSO (MOSTRAR PLANILHAS EXISTENTES)
+book = openpyxl.Workbook()
 print(book.sheetnames)
 
-# 3° PASSO (CRIAR NOVA PLANILHA)
-book.create_sheet('DADOS02')
+book.create_sheet("frutas")
 print(book.sheetnames)
 
-# 4° PASSO (COLOCAR NOVOS CONTEÚDOS NA PLANILHA DADOS02)
-DADOS02_page = book["DADOS02"]
-DADOS02_page.append(["NOME", "IDADE", "SÉRIE"])
-DADOS02_page.append(["Maria", "19", "3°ano"])
-DADOS02_page.append(["Pedro", "18", "2°ano"])
-DADOS02_page.append(["Lucas", "21", "5°ano"])
+book.create_sheet("bebidas")
+print(book.sheetnames)
 
-# 5° PASSO (MOSTRAR O CONTEÚDO DA PLANILHA DADOS01)
-DADOS02_page = book["DADOS02"]
-for rows in DADOS02_page.iter_rows(min_row=1, max_row=7):
-    print(rows[0].value, rows[1].value, rows[2].value)
+book["frutas"]
 
-# 6° PASSO (SALVAR O ARQUIVO)
-book.save('DADOS.xlsx')
+frutas_page = book["frutas"]
+
+frutas_page.append(["Frutas", "Quantidade", "Preço"])
+frutas_page.append(["Melancia", "12", "R$11,99"])
+frutas_page.append(["Maçã", "07", "R$7,89"])
+frutas_page.append(["Carambola", "23", "R$3,90"])
+
+book["bebidas"]
+
+bebidas_page = book["bebidas"]
+
+bebidas_page.append(["Bebidas", "Quantidade", "Preço"])
+bebidas_page.append(["Coca-Cola", "25", "R$9,89"])
+bebidas_page.append(["Fanta Laranja", "17", "R$7,90"])
+bebidas_page.append(["Chá Matte", "13", "R$5,60"])
+
+
+
+def colorizar_celulas("Panilha de Compras.xlsx", "B2", "0000FF"):
+    planilha['B2'].fill = PatternFill(start_color='0000FF', end_color='0000FF')
+
+arquivo_excel = openpyxl.load_workbook('Panilha de Compras.xlsx')
+
+# Escolhendo a planilha onde as células serão coloridas
+planilha = arquivo_excel.active
+
+# Colorindo a célula
+colorizar_celulas("Panilha de Compras.xlsx', 'B2', '0000FF")
+
+
+book.save("Panilha de Compras.xlsx")
+
+
